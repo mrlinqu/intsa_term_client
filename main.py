51b705fce3c72c33987426912cf87b8d863fb51c
r.khuramshin <no@ma.il>
2020-07-27 10:17:56 +0300

    + copyright header

--- main.py
+++ main.py
@@ -1,3 +1,10 @@
+# Copyright 2020 by Roman Khuramshin <mr.linqu@gmail.com>.
+# All rights reserved.
+# This file is part of the Intsa Term Client - X2Go terminal client for Windows,
+# and is released under the "MIT License Agreement". Please see the LICENSE
+# file that should have been included as part of this package.
+
+
 from MainWindow import MainWindow
 from X2goClient import X2goClient
 import os
